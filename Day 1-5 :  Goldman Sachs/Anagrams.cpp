9d99439e05a98363a94506efa47f6ac244ecbc03
Raja Sharma <85919555+rajasharma123q@users.noreply.github.com>
2022-01-02 08:59:34 +0530

    Update Anagrams.cpp

--- Day 1-5 :  Goldman Sachs/Anagrams.cpp
+++ Day 1-5 :  Goldman Sachs/Anagrams.cpp
@@ -1,1 +1,25 @@
 
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+   
+    map<string,vector<string>>mpp;
+    for(int i = 0; i<string_list.size(); i++){
+        string s = string_list[i];
+        sort(s.begin(),s.end());
+        mpp[s].push_back(string_list[i]);
+    }
+    
+    vector<vector<string>> answers(mpp.size());
+    int index = 0;
+    for(auto x : mpp){
+        auto v = x.second;
+        
+        for(int i =0 ; i<v.size(); i++){
+            answers[index].push_back(v[i]);
+        }
+        index++;
+    }
+    return answers;
+    }
+};
